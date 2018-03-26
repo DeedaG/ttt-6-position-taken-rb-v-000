c8c19839cbfc42d8ec4fdfd0af2c38db903e4dca
Christia Tidwell <christiatidwell@bellsouth.net>
2018-03-26 19:03:37 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,10 @@ def position_taken?(board,index)
   if board[index] == " "
     false
   elsif board[index] == ""
+    false
   elsif board[index] == nil
+    false
   else board[index] == "X" || "O"
+    true
   end
 end
